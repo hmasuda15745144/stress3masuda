f44c1b1875f2a50c82ea965457338b3a795601a7
prius20 <a1544hm@aiit.ac.jp>
2017-07-30 06:49:51 +0000

    helo cont. mode

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,9 +1,11 @@
 class HeloController < ApplicationController
 	layout 'application'
 	before_action :authenticate_account!, only: :login_check
+#	before_action :authenticate_account!
 
 	def index
 		@msg = 'this is sample page.'
+		@data = Helo.all
 	end
 	
 	def login_check
@@ -12,18 +14,50 @@ class HeloController < ApplicationController
 	end
 
   def add
-    		@msg = 'this is sample page add.'
+    @msg = "add new data."
+    @helo = helo.new
+  end
+
+  def create
+    @helo = Helo.new comment_params
+    if @helo.save then 
+          redirect_to '/helo'
+          return
+    end
+       render 'add'
   end
 
   def edit
-    		@msg = 'this is sample page edit.'
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @helo = Helo.find(params[:id])
+#    if request.patch? then 
+#       @helo.update(helo_params)
+#     goback
+#    end
   end
 
   def dell
     		@msg = 'this is sample page dell.'
+   obj = helo.find(helo[:id])
+   obj.update(helo_params)
+   redirect_to '/helos'
   end
+ 
 
   def list
     		@msg = 'this is sample page list.'
+    		@data = Helo.all
+###    @helo = Helo.find(params[:id])
+###    @data = Helo.find(params[:id])
   end
+
+private
+def helo_params
+   params.require(:helo).permit(:account_id, :kubun, :word )
+end
+
+def goback
+  redirect_to '/helo/index'
+end
+
 end
